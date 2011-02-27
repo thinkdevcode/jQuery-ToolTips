38e41a260378468d7de2a70b80da088936abfaa7
gene <gin4lyfe@gmail.com>
2011-02-27 16:40:40 -0500

    1.1.0
    - Pass in a url to an img and it will automatically show (I will add options to change size shortly)

--- jQueryToolTips.js
+++ jQueryToolTips.js
@@ -1,5 +1,5 @@
 ﻿// jQuery ToolTips
-// Version: 1.0.0, Last updated: 2/23/2011
+// Version: 1.1.0, Last updated: 2/27/2011
 //
 // GitHub       - https://github.com/thinkdevcode/jQuery-ToolTip
 // Contact      - gin4lyfe@gmail.com (Eugene Alfonso)
@@ -16,52 +16,68 @@
     *               a custom text provided in parameter.
     *
     *       options [object] [optional]
-    *           A map of settings... { container(string or object), text, width, 
-									   height, follow, offset { top, left } 
-									 }
-	*
+    *           A map of settings... { container(string or object), img, text, width, 
+    *                                  height, follow, offset { top, left } }
+    *
     */
     $.fn.tooltip = function (options) {
 
-        //this is our html markup for tooltip
-        var markup = '<div id="ttContainerDiv" class="ui-corner-all ui-widget-content ui-widget" style="padding:1em;display:none;position:fixed"></div>';
-
         var defaults = {
-			container: undefined,
+            container: undefined,
+            img: undefined,
             text: undefined,
             width: undefined,
             height: undefined,
             follow: undefined,
             offset: { top: 10, left: 15 }
         };
-		
-		$.extend(defaults, options); //apply any settings the user may have
-		
-		if (typeof defaults.container === 'string') {
-			$(markup).appendTo(defaults.container);
-		}
-		else if (typeof defaults.container === 'object') {
-			defaults.container.append(markup);
-		}
-		else {
-			$(markup).appendTo('body');
-		}
-		
-		var cont = $('#ttContainerDiv'); //quick access to our containter div
-		
+        $.extend(defaults, options); //apply any settings the user may have
+
+        var cont = $('#ttContainerDiv');
+
+        //create container if does not exist
+        if (!cont.length || cont.length <= 0) {
+
+            //this is our html markup for tooltip
+            var markup = '<div id="ttContainerDiv" class="ui-corner-all ui-widget-content ui-widget" style="padding:1em;display:none;position:fixed"> \
+                        <img src="#" id="ttImage" height="400px" width="250px" /><span id="ttText"></span></div>';
+
+            //append the container to other container (default 'body')
+            if (typeof defaults.container === 'string') {
+                $(markup).appendTo(defaults.container);
+            } else if (defaults.container instanceof jQuery) {
+                defaults.container.append(markup);
+            } else {
+                $(markup).appendTo('body');
+            }
+
+            cont = $('#ttContainerDiv');
+        }
+
+        var img = $('#ttImage');
+        var span = $('#ttText');
+
         //loop through our jquery objects
         this.each(function () {
 
             var tooltiptext = $(this).attr('title'); //grab the title for future use
 
-            //make sure we have some text to add (what would be a tooltip without text!)
-            if (tooltiptext || defaults.text) {
+            //make sure we have something for our tooltip
+            if (tooltiptext || defaults.text || defaults.img) {
                 $(this).hover(
                     function (e) { //mouseenter
 
                         $(this).attr('title', ''); //remove the title so original alt doesnt show
                         cont.css({ 'width': '', 'height': '' }); //reset the height and width
-                        cont.text(((defaults.text) ? defaults.text : tooltiptext));
+                        if (defaults.img) {
+                            img.attr('src', defaults.img);
+                            img.show();
+                            span.hide();
+                        } else {
+                            span.text(((defaults.text) ? defaults.text : tooltiptext));
+                            img.hide();
+                            span.show();
+                        }
 
                         //only change sizes if container is larger than it should be (this is why we reset the height and width)
                         if (defaults.width) { if (cont.width() > defaults.width) { cont.width(defaults.width); } }
@@ -71,17 +87,15 @@
                             $(this).mousemove(function (e) {
                                 cont.css({ 'top': e.clientY + defaults.offset.top, 'left': e.clientX + defaults.offset.left });
                             });
-                        }
-                        else {
+                        } else {
                             cont.css({ 'top': e.clientY + defaults.offset.top, 'left': e.clientX + defaults.offset.left });
                         }
                         cont.show();
                     },
                     function (e) { //mouseexit
-
                         if (defaults.follow) { $(this).unbind('mousemove'); }
                         cont.hide();
-                        cont.text(''); //reset text
+                        span.text(''); //reset text
                         $(this).attr('title', tooltiptext); //return title to what it was before
                     }
                 );
